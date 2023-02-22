2711fe5dc8ce6ea329cb9ba9db7c7d3ab74dc291
BenNguyen97 <114214306+BenNguyen97@users.noreply.github.com>
2023-02-22 18:43:00 +0700

    a

---
+++ final-test/src/App.js
@@ -0,0 +1,17 @@
+import Test1 from './data/test.1/test-1';
+import Test2 from './data/test.2/test-2';
+import Test3 from './data/test.3/test-3';
+
+function App() {
+  return (
+    <div >
+      <Test1/>
+      <hr/>
+      <Test2/>
+      <hr/>
+      <Test3/>
+    </div>
+  );
+}
+
+export default App;
